3950f54b20703c8fb4283a5972603acf30eee976
Bernardo Codesido <bernardo@rsk.co>
2016-10-21 10:28:50 -0300

    Read config from config file. Load address PK on script startup.

--- rsk-faucet.js
+++ rsk-faucet.js
@@ -2,13 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
-
-var rskNode = "localhost:4444";
-var faucetAddress = "0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826";
-var reCaptchaSecret = "reCaptchaSecretKey";
-var valueToSend = 10000000000000000;
-var gasPrice = 1;
-var gas = 21000;
+var fs = require('fs');
+var Web3 = require('web3');
 
 app.use('/css', express.static('css'));
 app.use('/img', express.static('img'));
@@ -20,29 +15,82 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
+var port;
+var rskNode;
+var faucetAddress;
+var reCaptchaSecret;
+var valueToSend;
+var gasPrice;
+var gas;
+var faucetPrivateKey;
 
-
-var fs = require('fs');
 eval(fs.readFileSync('lib/validate-rsk-address.js')+'');
 
-var Web3 = require('web3');
+readConfig();
+
 function getWeb3() {
-    if (web3)
-        return web3;
-
-    console.log('using web3', rskNode);
-    web3 = new Web3();
-    web3.setProvider(new web3.providers.HttpProvider('http://' + rskNode));
-
+  if (web3)
     return web3;
+
+  console.log('using web3', rskNode);
+  web3 = new Web3();
+  web3.setProvider(new web3.providers.HttpProvider('http://' + rskNode));
+
+  return web3;
 }
 var web3;
 getWeb3();
 
+extendWeb3();
+loadPk();
+
 
 function executeTransfer(destinationAddress) {
-    var result = web3.eth.sendTransaction({from: faucetAddress, to: destinationAddress, gasPrice: gasPrice, gas: gas, value: valueToSend});
-    console.log('transaction hash', result);
+  var result = web3.eth.sendTransaction({from: faucetAddress, to: destinationAddress, gasPrice: gasPrice, gas: gas, value: valueToSend});
+  console.log('transaction hash', result);
+}
+
+function readConfig(){
+  obj=JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+  port = obj.port;
+  rskNode = obj.rskNode;
+  faucetAddress = obj.faucetAddress;
+  faucetPrivateKey = obj.faucetPrivateKey;
+  reCaptchaSecret = obj.reCaptchaSecret;
+  valueToSend = obj.valueToSend;
+  gasPrice = obj.gasPrice;
+  gas = obj.gas;
+}
+
+function extendWeb3() {
+  web3._extend({
+    property: 'personal',
+    methods: [new web3._extend.Method({
+      name: 'importRawKey',
+      call: 'personal_importRawKey',
+      params: 2,
+      inputFormatter: [function (value) { return value; }, function (value) { return value; }],
+      outputFormatter: null
+    })]
+  });
+
+  web3._extend({
+    property: 'personal',
+    methods: [new web3._extend.Method({
+      name: 'unlockAccount',
+      call: 'personal_unlockAccount',
+      params: 3,
+      inputFormatter: [function (value) { return value; }, function (value) { return value; }, function (value) { return value; }],
+      outputFormatter: null
+    })]
+  });
+}
+
+function loadPk() {
+  console.log('Loding PK to node');
+  var result = web3.personal.importRawKey(faucetPrivateKey, "passPhraseToEncryptPrivKey");
+  var result = web3.personal.unlockAccount(faucetAddress, "passPhraseToEncryptPrivKey", "0");
+  console.log('PKs loaded to the node');
 }
 
 app.get('/balance', function (req, res) {
@@ -53,8 +101,8 @@ app.get('/balance', function (req, res) {
 
 app.post('/', function (req, res) {
   if (!validateRskAddress(req.body.rskAddress)) {
-      console.log('Invalid RSK address format ', req.body.rskAddress);
-      return res.status(400).send('Invalid RSK address format.');
+    console.log('Invalid RSK address format ', req.body.rskAddress);
+    return res.status(400).send('Invalid RSK address format.');
   }
   // gRecaptchaResponse is the key that browser will generate upon form submit.
   // if its blank or null means user has not selected the captcha, so return the error.
@@ -81,6 +129,6 @@ app.post('/', function (req, res) {
   res.send('Successfully sent some SBTCs to ' + req.body.rskAddress + '.');
 });
 
-app.listen(3000, function () {
-  console.log('RSK Faucet started on port 3000');
+app.listen(port, function () {
+  console.log('RSK Faucet started on port ' + port);
 });
